a5fd70bec684e93ff276333184a9c18fe4b6d56a
x300n <houseofxeon@gmail.com>
2020-07-27 05:31:13 +0200

    Intialize

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
